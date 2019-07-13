21474e107007f1b4c2c293760054afef20bcceb1
Radovan Djogo <radovan@stretch.htecgroup.com>
2019-07-13 22:30:49 +0200

    Set default value to None instead of an empty list

--- actions/send_email.py
+++ actions/send_email.py
@@ -8,7 +8,7 @@ from email.mime.text import MIMEText
 
 
 class SendEmail(Action):
-    def run(self, email_from, email_to, subject, message, account, email_cc=[], mime="plain", attachments=None):
+    def run(self, email_from, email_to, subject, message, account, email_cc=None, mime="plain", attachments=None):
 
         if mime not in ['plain', 'html']:
             raise ValueError('Invalid mime provided: ' + mime)
@@ -32,9 +32,13 @@ class SendEmail(Action):
         msg['Subject'] = Header(subject, 'utf-8')
         msg['From'] = email_from
         msg['To'] = ", ".join(email_to)
-        msg['Cc'] = ", ".join(email_cc)
         msg.attach(MIMEText(message, mime, 'utf-8'))
-        email_to += email_cc
+        if email_cc is None: 
+            email_cc = []
+
+        if len(email_cc) > 0:
+            msg['Cc'] = ", ".join(email_cc)
+            email_to += email_cc
 
         attachments = attachments or tuple()
         for filepath in attachments:
